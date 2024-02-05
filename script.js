6b8c0ae5cd5b0adf81d9a6a5b0ea4cdc8941fbdf
Abhishek621 <147933760+Abhishek6212@users.noreply.github.com>
2024-02-05 21:31:19 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuButton = document.querySelector('.menu-button');
-    const menu = document.querySelector('.menu');
-
-    menuButton.addEventListener('click', function () {
-        menuButton.classList.toggle('active');
-        menu.classList.toggle('show');
-    });
-});
